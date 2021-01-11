8fccec3dedc47a8393558ec7e6bc27a684656184
Dumisani <dumisanik.shabangu@gmail.com>
2021-01-11 18:33:08 +0200

    Configured Firebase & Add post to firestore

--- src/components/CreateNewPost.js
+++ src/components/CreateNewPost.js
@@ -1,33 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const CreateNewPost = props => {
-    return (
-        <>
-            <form onSubmit={props.savePost}>
-                <h1>Create New Post</h1>
-                <input
-                    type="text"
-                    onChange={props.savePostTitleToState}
-                    placeholder="title"
-                    size="39"
-                    required
-                    ref={props.getTitle}
-                ></input>
-                <br />
-                <br />
-                <textarea
-                    onChange={props.savePostContentToState}
-                    placeholder="contents"
-                    rows="8"
-                    cols="41"
-                    required
-                    ref={props.getContent}
-                ></textarea>
-                <br />
-                <br />
-                <button>Save Post</button>
-            </form>
-        </>
-    );
+import { firestore } from "../utilities/firebase";
+
+const initialState = {
+  title: "",
+  content: "",
+  error: null,
+};
+
+const CreateNewPost = (props) => {
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { title, content } = state;
+
+    const post = {
+      title,
+      content,
+      createdAt: new Date(),
+    };
+
+    if (!title || !content) {
+      console.log("empty content!");
+    } else {
+      await firestore
+        .collection("posts")
+        .add(post)
+        .catch((error) => {
+          setState({ ...state, error: error });
+          console.error(error);
+        });
+    }
+
+    clear();
+  };
+
+  const clear = () => {
+    setState(initialState);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Create New Post</h1>
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          placeholder="title"
+          size="39"
+          // required
+          value={state.title}
+        ></input>
+        <br />
+        <br />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={state.content}
+          placeholder="contents"
+          rows="8"
+          cols="41"
+          //  required
+        ></textarea>
+        <br />
+        <br />
+        <button>Save Post</button>
+      </form>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </>
+  );
 };
 export default CreateNewPost;
